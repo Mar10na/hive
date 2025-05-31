eb799eb1ffb2013a426b09278ddc39c251236033
Mar10na <yagonamadmusayeva@gmail.com>
2025-05-31 17:59:32 +0500

    delelate country_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive_project/newproject/countriesModel.dart';
+//import 'package:hive_project/newproject/countriesModel.dart';
 
 void main() {
   runApp(const MyApp());
